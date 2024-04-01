42e84561472ca3ae9dad90cefcc9ffbeae152a90
mueed98 <mueedmubashar98@gmail.com>
2024-04-01 17:04:03 +0500

    verification added

--- hardhat/ignition/modules/MedicalRecords.ts
+++ hardhat/ignition/modules/MedicalRecords.ts
@@ -1,9 +1,12 @@
 import { buildModule } from "@nomicfoundation/hardhat-ignition/modules";
 
-const MedicalRecordsModule = buildModule("MedicalRecordsModule", (m) => {
-	const MedicalRecords = m.contract("MedicalRecords");
+const MedicalRecordsModule = buildModule(
+	`MedicalRecords_${Date.now()}`,
+	(m) => {
+		const MedicalRecords = m.contract("MedicalRecords");
 
-	return { MedicalRecords };
-});
+		return { MedicalRecords };
+	}
+);
 
 export default MedicalRecordsModule;
